49b403d4a8438d531972da4cccf121fd64ac5f16
wblu214 <wblu.eric@gmail.com>
2026-01-25 20:23:05 +0800

    add some demo

--- linkedList/initLinkedListNode.go
+++ linkedList/initLinkedListNode.go
@@ -14,6 +14,7 @@ func CreateLinkedNode(sourceArray []int) *ListNode {
 	}
 	head := &ListNode{Val: sourceArray[0]}
 	cur := head
+
 	for i := 1; i < len(sourceArray); i++ {
 		node := &ListNode{Val: sourceArray[i], Next: nil}
 		cur.Next = node
