a4025cb115e78873612ecfce50588280da03ac5f
wblu214 <wblu.eric@gmail.com>
2026-02-14 18:28:28 +0800

    add some demo

--- character/findLongestSubCharacter.go
+++ character/findLongestSubCharacter.go
@@ -45,7 +45,7 @@ func lengthOfLongestSubstring(s string) int {
 }
 
 func main() {
-	//www
+	//wwww
 	source := "abcabcbb"
 	fmt.Printf("原字符串为：%s\n", source)
 	result := lengthOfLongestSubstring(source)
